c759f4451af225b8288e030d431fcaacaec7715d
Maria Sharkova <moriko.tys@gmail.com>
2023-09-21 15:17:12 +0300

    second review

--- app/api/routers.py
+++ app/api/routers.py
@@ -4,16 +4,16 @@ from app.api.endpoints import (
     user_router, charity_project_router, donation_router
 )
 
-main_router = APIRouter()
+main_router_v1 = APIRouter()
 
-main_router.include_router(
+main_router_v1.include_router(
     charity_project_router,
     prefix='/charity_project',
     tags=['Charity Projects']
 )
 
-main_router.include_router(user_router)
+main_router_v1.include_router(user_router)
 
-main_router.include_router(
+main_router_v1.include_router(
     donation_router, prefix='/donation', tags=['Donations']
 )
--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-from app.api.routers import main_router
+from app.api.routers import main_router_v1
 from app.core.config import settings
 from app.core.init_db import create_first_superuser
 
 
 app = FastAPI(title=settings.app_title, description=settings.app_description)
-app.include_router(main_router)
+app.include_router(main_router_v1)
 
 
 @app.on_event('startup')
